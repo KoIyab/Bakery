e20614f3439ce411cf32b8b36e7b3fdf7cc04440
Kolyab <102136739+KoIyab@users.noreply.github.com>
2022-03-23 10:54:44 +0000

    n

--- Main.java
+++ Main.java
@@ -1,16 +1,20 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world!"); }
+  public static void makeButter(){
     System.out.println("Mix the dry ingredients.");
     System.out.println("Cream the butter and sugar.");
     System.out.println("Beat in the eggs.");
     System.out.println("Stir in the dry ingredients.");
+    }
+    public static void makeCookies(){
     System.out.println("Set the oven temperature.");
     System.out.println("Set the timer.");
     System.out.println("Place a batch of cookies into the oven.");
     System.out.println("Allow the cookies to bake.");
+    }
+    public static void decorate(){
 		System.out.println("Mix ingredients for frosting.");
     System.out.println("Spread frosting and sprinkles.");
-
   }
 }
